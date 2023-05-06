b1a5d32c91cc126bd91229df498bc4908cc1d1fc
KarsteenPW <127945125+KarsteenPW@users.noreply.github.com>
2023-05-06 09:18:01 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          PTI NYOBA PAKE VERCEL
         </a>
       </header>
     </div>
